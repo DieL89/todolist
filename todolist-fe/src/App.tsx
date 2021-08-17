8cde545b953339b3301ca4d1b69865dcebc3f52f
DieL89 <dzmitry273@gmail.com>
2021-08-17 20:53:57 +0300

    fix: fix main file

--- todolist-fe/src/App.tsx
+++ todolist-fe/src/App.tsx
@@ -1,25 +1,12 @@
-import React, { useState, useRef }  from 'react';
-import { useEffect } from 'react';
+import React from 'react';
 import TodosPage from './pages/TodoPage';
 
 function App(): JSX.Element {
-  const [userName, changeUserName] = useState<string>('');
-  const [password, changePassword] = useState<string>('');
-
   return (
     <div className="container">
-      <Input handler={(e: any) => changeUserName(e.target.value)} value={userName} />
-      <Input handler={(e: any) => changePassword(e.target.value)} value={password} />
-      {/* <input onChange={(e) => changeUserName(e.target.value)} type="text" name="username" value={userName} />
-      <input ref={passwordRef} onChange={(e) => changePassword(e.target.value)} type="text" name="password" value={password} /> */}
+      <TodosPage />
     </div>
   );
-}
-
-function Input(props: any) {
-  console.log(props.value);
-
-  return (<input type="text" name={props.value} onChange={props.handler} />)
 }
 
 export default App;
